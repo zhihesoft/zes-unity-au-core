3134fd144b8d6aeed0be76552d38a7f667a719fe
stanley <stanley@zhihesoft.com>
2022-10-18 15:17:19 +0800

    add asset set test

---
+++ Assets/Tests/AssetSetTest.cs
@@ -0,0 +1,19 @@
+﻿using Au;
+using NUnit.Framework;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal class AssetSetTest
+{
+    [Test]
+    public async Task TestForAssetSet()
+    {
+        AssetSet ass = new AssetSet("");
+        await ass.LoadBundle("languages", null);
+        var obj = await ass.LoadObject(
+            Path.Combine("Assets", "Bundles", "languages", "i18n-zh-cn.json"),
+            typeof(TextAsset));
+        Assert.IsNotNull(obj);
+    }
+}
